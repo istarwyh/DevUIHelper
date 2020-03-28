e26738746218d9d896d6500189c7311224fbe146
zhangke98 <1900022733@pku.edu.cn>
2020-03-28 16:20:12 +0800

    修复事件换行不显示

--- src/hoverEvent.ts
+++ src/hoverEvent.ts
@@ -1,6 +1,6 @@
 'use strict';
 import {
-     TextDocument,languages, CompletionItem, Position, CompletionItemKind, MarkdownString, SnippetString
+     TextDocument,languages, CompletionItem, Position, CompletionItemKind, MarkdownString, SnippetString, Range
 } from 'vscode';
 // 下面这个语句导入一个文件夹模块,入口在index
 import components from './params_Event';
@@ -9,9 +9,10 @@ import * as _getName from './util/getName';
 const completionTriggerChars = [" ", "\n"];  
 function  provideCompletionItems(document: TextDocument, position: Position): CompletionItem[] {
 
+    const start: Position = new Position(0, 0);
+    const range: Range = new Range(start, position);
+    const text = document.getText(range);
     /* 减少检索范围，仅检索光标所在行 */ 
-    const line = document.lineAt(position);
-    const text = line.text.substring(0,position.character);
      
     const componentRegex = /<(d-[a-zA-Z0-9-]*)\b[^<>]*$/g;
     
