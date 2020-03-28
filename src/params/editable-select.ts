a05eaedf91d473549e707546255af9f8dd0e75d7
zhangke98 <1900022733@pku.edu.cn>
2020-03-28 18:24:25 +0800

    更新8个不全的组件属性和事件

--- src/params/editable-select.ts
+++ src/params/editable-select.ts
@@ -1,3 +1,13 @@
 export default{
-ngModel:     '|any|--|可选，绑定选中对象，可双向绑定|',
+ngModel             :"| `any`          | --    | 可选，绑定选中对象，可双向绑定                    |",
+ngModelChange       :"| `EventEmitter` | --    | 可选，仅支持事件绑定，用于处理选中对象发生变化       |",
+source              :"| `any[]`        | --   | 必选，菜单的条目                             |",
+disabled            :"| `boolean`      | false     | 可选，值为true禁用下拉框         |",
+disabledKey         :"   | `string`      | --     | 可选，设置禁用选项的Key值         |",
+placeholder         :"| `string`       | ''        | 可选，没有选中项的时候提示文字    |",
+itemTemplate        :"| `TemplateRef`  | --      | 可选，下拉菜单条目的模板           ｜",
+noResultItemTemplate:"| `TemplateRef`  | --      | 可选，下拉菜单条目搜索后没有结果的模版|",
+maxHeight           :"| `number`       | --      | 可选，下拉菜单的最大高度           ｜",
+searchFn            :"| `Function`      | (term: string) => Observable<any[]> | 可选，自定义搜索函数|",
+enableLazyLoad      :"      | `boolean`      | false | 可选，是否允许懒加载  |",
 };
--- src/params_Event/select.ts
+++ src/params_Event/select.ts
@@ -1,2 +1,5 @@
 export default{
+valueChange :"| `EventEmitter<Array<any>|any>`      | 可选,输出函数,当选中某个选项项后,将会调用此函数,参数为当前选择项的值 |",
+toggleChange:"| `EventEmitter<boolean>`         | 可选,输出函数,下拉打开关闭toggle事件                              |",
+loadMore    :"| `EventEmitter<{instance: Selectcomponent, event: ScrollEvent}>`          | 懒加载触发事件，配合`enableLazyLoad`使用，使用`$event.instance.loadFinish()`结束本次加载, event为懒加载监听的滚动事件，参考dLazyLoad |",
 };
